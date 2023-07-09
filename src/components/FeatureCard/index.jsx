c738030e91c600da175fefaeb18849b7834d3d39
Joiyad <20bee012@nith.ac.in>
2023-07-09 20:47:22 +0530

    card size adjusted

--- src/components/FeatureCard/index.jsx
+++ src/components/FeatureCard/index.jsx
@@ -6,7 +6,7 @@ const FeatureCard = ({title, description, imageLink}) => {
   return (
     <div className={styles.card_container}>
         <div className={styles.image_container}><img alt="logo" src={imageLink} width="100px" /></div>
-        <Typography variant='h5' sx={{textAlign:"center", marginBottom:'20px'}}>{title}</Typography>
+        <Typography variant='h6' sx={{textAlign:"center", marginBottom:'20px'}}>{title}</Typography>
         <Typography variant='body2' sx={{color:'#7F7E7F', textAlign:"center"}}>{description}</Typography>
     </div>
   )
